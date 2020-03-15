cbf82f914bcd19b2e8c748f698da4e99ebb0f640
Aaron Traas <aaron@traas.org>
2020-03-15 01:33:18 -0400

    Propper HTTP verbs

--- service.go
+++ service.go
@@ -8,7 +8,6 @@ import (
 )
 
 type Response struct {
-	Path    string      `json:"path"`
 	Success bool        `json:"success"`
 	Status  int         `json:"status"`
 	Quote   *QuoteEntry `json:"quote,omitempty"`
@@ -34,10 +33,20 @@ func sendJsonResponse(w http.ResponseWriter, response Response) {
 	fmt.Fprintf(w, string(responseJson))
 }
 
-func setApprovalResponse(quotes *QuoteList, path string, idString string, approved bool) Response {
+func setApprovalResponse(quotes *QuoteList, r *http.Request, approved bool) Response {
+	if r.Method != http.MethodPost {
+		return Response{
+			Success: false,
+			Status:  http.StatusMethodNotAllowed,
+			Error:   fmt.Sprintf("Method %s not allowed. Must be POST.", r.Method),
+		}
+	}
+
+	r.ParseForm()
+	idString := r.Form.Get("id")
+
 	if idString == "" {
 		return Response{
-			Path:    path,
 			Success: false,
 			Status:  http.StatusBadRequest,
 			Error:   "'id' is required parameter",
@@ -46,7 +55,6 @@ func setApprovalResponse(quotes *QuoteList, path string, idString string, approv
 	id, errIdConvert := strconv.Atoi(idString)
 	if errIdConvert != nil {
 		return Response{
-			Path:    path,
 			Success: false,
 			Status:  http.StatusBadRequest,
 			Error:   "'id' is not an integer",
@@ -56,7 +64,6 @@ func setApprovalResponse(quotes *QuoteList, path string, idString string, approv
 	quote, success := quotes.setApproval(id, approved)
 	if success == false {
 		return Response{
-			Path:    path,
 			Success: false,
 			Status:  http.StatusNotFound,
 			Error:   fmt.Sprintf("quote #%d not found", id),
@@ -64,33 +71,44 @@ func setApprovalResponse(quotes *QuoteList, path string, idString string, approv
 	}
 
 	return Response{
-		Path:    path,
 		Success: true,
 		Quote:   quote,
 	}
 }
 
 func (app *QuoteApplication) rootHandler(w http.ResponseWriter, r *http.Request) {
-	var response Response
 
-	if r.Method == http.MethodGet {
-		response = Response{
-			Path:    r.URL.Path,
-			Success: true,
-			Quotes:  &app.Quotes,
-		}
+	if r.Method != http.MethodGet {
+		sendJsonResponse(w, Response{
+			Success: false,
+			Status:  http.StatusMethodNotAllowed,
+			Error:   fmt.Sprintf("Method %s not allowed. Must be GET.", r.Method),
+		})
+		return
 	}
 
-	sendJsonResponse(w, response)
+	sendJsonResponse(w, Response{
+		Success: true,
+		Quotes:  &app.Quotes,
+	})
 }
 
 func (app *QuoteApplication) submitHandler(w http.ResponseWriter, r *http.Request) {
-	bodyString := r.URL.Query().Get("body")
-	urlString := r.URL.Query().Get("url")
+	if r.Method != http.MethodPost {
+		sendJsonResponse(w, Response{
+			Success: false,
+			Status:  http.StatusMethodNotAllowed,
+			Error:   fmt.Sprintf("Method %s not allowed. Must be POST.", r.Method),
+		})
+		return
+	}
+
+	r.ParseForm()
+	bodyString := r.Form.Get("body")
+	urlString := r.Form.Get("url")
 
 	if (bodyString == "") || (urlString == "") {
 		sendJsonResponse(w, Response{
-			Path:    r.URL.Path,
 			Status:  http.StatusBadRequest,
 			Success: false,
 			Error:   "'body' and 'url' are require parameters",
@@ -108,7 +126,6 @@ func (app *QuoteApplication) submitHandler(w http.ResponseWriter, r *http.Reques
 	app.Quotes = append(app.Quotes, quote)
 
 	sendJsonResponse(w, Response{
-		Path:    r.URL.Path,
 		Status:  http.StatusCreated,
 		Success: true,
 		Quote:   &quote,
@@ -116,9 +133,9 @@ func (app *QuoteApplication) submitHandler(w http.ResponseWriter, r *http.Reques
 }
 
 func (app *QuoteApplication) approveHandler(w http.ResponseWriter, r *http.Request) {
-	sendJsonResponse(w, setApprovalResponse(&app.Quotes, r.URL.Path, r.URL.Query().Get("id"), true))
+	sendJsonResponse(w, setApprovalResponse(&app.Quotes, r, true))
 }
 
 func (app *QuoteApplication) disapproveHandler(w http.ResponseWriter, r *http.Request) {
-	sendJsonResponse(w, setApprovalResponse(&app.Quotes, r.URL.Path, r.URL.Query().Get("id"), false))
+	sendJsonResponse(w, setApprovalResponse(&app.Quotes, r, false))
 }
